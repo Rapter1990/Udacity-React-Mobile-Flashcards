e323af733444a8e33ace2f082cb2e47e5b036683
Noyan <sngermiyanoglu@hotmail.com>
2021-01-22 11:35:45 +0300

    Task 12 : Get All Initial Values of Deck in App.js

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import setNotification from './utils/notification';
 import store from './store'
+import getAllDecks from './actions/index'
 
 export default class App extends React.Component {
   
   componentDidMount() {
+    store.dispatch(getAllDecks());
     setNotification();
   }
   render() {
