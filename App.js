106c05952ae8cf4873682c264580b6f375c595f3
Noyan <sngermiyanoglu@hotmail.com>
2021-01-22 11:31:46 +0300

    Task 11 : Defined Notification and Store in App.js

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import setNotification from './utils/notification';
+import store from './store'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  
+  componentDidMount() {
+    setNotification();
+  }
+  render() {
+      return (
+          <Provider store={store}>
+              <View style={styles.container}>
+                  
+              </View>
+          </Provider>
+      );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+      flex: 1,
+      backgroundColor: '#dde'
+  }
 });
