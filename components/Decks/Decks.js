faa2b05eb613232d508fd45799152e594ff09f8b
Noyan <sngermiyanoglu@hotmail.com>
2021-01-23 15:01:02 +0300

    Task 25 : Show all decks in Decks Component and Devised each one in Deck Component

--- components/Decks/Decks.js
+++ components/Decks/Decks.js
@@ -3,6 +3,7 @@ import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-nati
 import { getAllDecks } from '../../actions';
 import { connect } from 'react-redux';
 import Deck from '../Deck';
+import { gray, lightPurp } from '../../utils/colors';
 
 
 class Decks extends Component {
@@ -15,13 +16,52 @@ class Decks extends Component {
 
         const { decks, navigation} = this.props;
 
+        const deckListing = Object.values(decks).map(deck => {
+            return (
+                <TouchableOpacity
+                    key={deck.title}
+                    onPress={() =>
+                        navigation.navigate('DeckDetail', { title: deck.title })
+                    }
+                >
+                  <Deck id={deck.title} />
+                </TouchableOpacity>
+            );
+        })
+
         return (
-            <View>
-                <Text>Decks</Text>
-            </View>
-        )
+            <ScrollView style={styles.container}>
+              <Text style={styles.title}>Udacity Mobile Flashcards</Text>
+              <Text style={styles.subTitle}>Decks</Text>
+              {deckListing}
+              <View style={{ marginBottom: 30 }} />
+            </ScrollView>
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    title: {
+      fontSize: 40,
+      textAlign: 'center',
+      marginBottom: 16,
+      color: gray
+    },
+    subTitle: {
+      fontSize: 32,
+      textAlign: 'center',
+      marginBottom: 16,
+      color: gray
+    },
+    container: {
+      flex: 1,
+      paddingTop: 16,
+      paddingRight: 16,
+      backgroundColor: lightPurp,
+      paddingLeft: 16,
+      paddingBottom: 16,
+    },
+});
 
 // refer to reducers/index.js
 const mapStateToProps = state => ({ decks: state });
