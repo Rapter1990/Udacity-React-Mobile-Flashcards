9e25657bc19fd37c160a376fa39b6685402226b3
Noyan <sngermiyanoglu@hotmail.com>
2021-01-23 12:26:42 +0300

    Task 23 : Created Deck Component and Defined props in Decks Component

---
+++ components/Deck/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default class Deck extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Deck</Text>
+            </View>
+        )
+    }
+}
--- components/Decks/Decks.js
+++ components/Decks/Decks.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react'
 import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { getAllDecks } from '../../actions';
 import { connect } from 'react-redux';
+import Deck from '../Deck';
 
 
 export default class Decks extends Component {
 
     componentDidMount() {
         this.props.getAllDecks();
-      }
-
-
+    }
+    
     render() {
+
+        const { decks } = this.props;
+        console.log(decks);
+
         return (
             <View>
                 <Text>Decks</Text>
