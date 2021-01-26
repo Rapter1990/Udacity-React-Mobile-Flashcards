de9c15e28aa8a480416a67bbe39ee6cb29526633
Noyan <sngermiyanoglu@hotmail.com>
2021-01-26 10:55:22 +0300

    Task 35 : Created QuizError and QuizResult Component, Added cases for QuizError and QuizResult in Quiz Component

--- components/Quiz/Quiz.js
+++ components/Quiz/Quiz.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { clearLocalNotification, setLocalNotification } from '../../utils/notification';
+import QuizError from "./QuizError";
+import QuizResult from './QuizResult';
 
 class Quiz extends Component {
 
@@ -9,6 +11,8 @@ class Quiz extends Component {
         title: '',
         currentQuestion: '',
         currentAnswer: '',
+        correctAnswer: 0,
+        incorrectAnswer: 0,
         questionNumber: 0,
         numberOfQuestions: this.props.deck.questions.length,
         score: 0,
@@ -20,6 +24,8 @@ class Quiz extends Component {
           title: this.state.questions[0].title,  
           currentQuestion: this.state.questions[0].question,
           currentAnswer: '',
+          correctAnswer: 0,
+          incorrectAnswer: 0,
           questionNumber: 1,
           score: 0,
           quizFinished: false,
@@ -45,10 +51,30 @@ class Quiz extends Component {
 
     render() {
 
-        const { navigation } = this.props;
+        const { navigation, questions} = this.props;
         const title = navigation.getParam('title', 'undefined');
 
         console.log(title);
+
+        if (questions.length === 0) {
+            return <QuizError />;
+        }
+
+        if (this.state.quizFinished == true) {
+            const { correctAnswer, incorrectAnswer , numberOfQuestions } = this.state;
+            const scrorePercentValue = ((correctAnswer / numberOfQuestions) * 100).toFixed(0);
+
+            return (
+                <QuizResult
+                    deck={deck}
+                    navigation={this.props.navigation}
+                    handleReset={this.resetQuiz}
+                    percent={scrorePercentValue}
+                    coorectAnswer = {correctAnswer}
+                    incorrectAnswer = {incorrectAnswer}
+                />
+            );
+        }
 
         return (
             <View>
---
+++ components/Quiz/QuizResult.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class QuizResult extends Component {
+    render() {
+
+        const {deck, navigation, handleReset, percent, coorectAnswer , incorrectAnswer } = this.props;
+
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default QuizResult;
